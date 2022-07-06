8f2dae6c8f3fb97c866a5881bea5cf9328405611
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-06 16:46:04 +0900

    [add] #1 - SignIn, Main Activity 초기화

---
+++ app/src/main/java/org/woowatechcamp/mailapplication/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.woowatechcamp.mailapplication.presentation
+
+import android.os.Bundle
+import org.woowatechcamp.mailapplication.R
+import org.woowatechcamp.mailapplication.databinding.ActivityMainBinding
+import org.woowatechcamp.mailapplication.util.base.BindingActivity
+
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/org/woowatechcamp/mailapplication/presentation/auth/SignInActivity.kt
@@ -0,0 +1,12 @@
+package org.woowatechcamp.mailapplication.presentation.auth
+
+import android.os.Bundle
+import org.woowatechcamp.mailapplication.R
+import org.woowatechcamp.mailapplication.databinding.ActivitySignInBinding
+import org.woowatechcamp.mailapplication.util.base.BindingActivity
+
+class SignInActivity : BindingActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
