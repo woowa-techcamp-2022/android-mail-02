ccb3b273edf1fab9efeb41d774e65389cbdaa52a
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-08 04:53:54 +0900

    [ADD] #6 - 화면전환 확장함수 추가

--- app/src/main/java/org/woowatechcamp/mailapplication/util/ext/ContextExt.kt
+++ app/src/main/java/org/woowatechcamp/mailapplication/util/ext/ContextExt.kt
@@ -1,16 +1,25 @@
 package org.woowatechcamp.mailapplication.util.ext
 
+import android.app.Activity
 import android.content.Context
-import android.content.Context.INPUT_METHOD_SERVICE
+import android.content.Intent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-
-fun Context.showKeyboard(view: View) {
-    val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-    inputMethodManager.showSoftInput(view.findFocus(), 0)
-}
+import androidx.core.os.bundleOf
 
 fun Context.closeKeyboard(view: View) {
     val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+inline fun <reified T : Activity> Context.buildIntent(
+    vararg argument: Pair<String, Any?>
+) = Intent(this, T::class.java).apply {
+    putExtras(bundleOf(*argument))
+}
+
+inline fun <reified T : Activity> Context.startActivity(
+    vararg argument: Pair<String, Any?>
+) {
+    startActivity(buildIntent<T>(*argument))
 }
