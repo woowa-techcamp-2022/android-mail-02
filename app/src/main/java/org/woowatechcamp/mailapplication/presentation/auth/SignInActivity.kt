8e53e7648eff7a34a398aeb97805c9184367acfa
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-08 06:23:17 +0900

    [FEAT] #3 - 로그인 뷰로직 구현

--- app/src/main/java/org/woowatechcamp/mailapplication/presentation/auth/SignInActivity.kt
+++ app/src/main/java/org/woowatechcamp/mailapplication/presentation/auth/SignInActivity.kt
@@ -1,23 +1,82 @@
 package org.woowatechcamp.mailapplication.presentation.auth
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.MotionEvent
 import android.widget.EditText
+import androidx.activity.viewModels
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import org.woowatechcamp.mailapplication.R
+import org.woowatechcamp.mailapplication.data.model.EMAIL
+import org.woowatechcamp.mailapplication.data.model.NICKNAME
 import org.woowatechcamp.mailapplication.databinding.ActivitySignInBinding
 import org.woowatechcamp.mailapplication.presentation.MainActivity
+import org.woowatechcamp.mailapplication.util.LoginCheckPatternUtil.isNotValidEmail
+import org.woowatechcamp.mailapplication.util.LoginCheckPatternUtil.isNotValidName
 import org.woowatechcamp.mailapplication.util.base.BindingActivity
 import org.woowatechcamp.mailapplication.util.ext.closeKeyboard
+import org.woowatechcamp.mailapplication.util.ext.startActivity
 
 class SignInActivity : BindingActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
+    private val viewModel by viewModels<AuthViewModel>()
+    private var isNickNameValid = false
+    private var isEmailValid = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.btnNext.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+        initClickEvent()
+        initTextChangeListener()
+        observeData()
+    }
+
+    private fun initClickEvent() {
+        with(binding) {
+            btnNext.setOnClickListener {
+                startActivity<MainActivity>(
+                    Pair(EMAIL, etEmail.text.toString()),
+                    Pair(NICKNAME, etNickname.text.toString())
+                )
+                finish()
+            }
         }
     }
+
+    private fun initTextChangeListener() {
+        with(binding) {
+            etNickname.addTextChangedListener {
+                if (isNotValidName(etNickname.text.toString())) {
+                    layoutNickname.error = getString(R.string.nickname_error)
+                    isNickNameValid = false
+                } else {
+                    layoutNickname.error = null
+                    isNickNameValid = true
+                }
+                viewModel.setNextBtnEnable(isValid())
+            }
+            etEmail.addTextChangedListener {
+                if (isNotValidEmail(etEmail.text.toString())) {
+                    layoutEmail.error = getString(R.string.email_error)
+                    isEmailValid = false
+                } else {
+                    layoutEmail.error = null
+                    isEmailValid = true
+                }
+                viewModel.setNextBtnEnable(isValid())
+            }
+        }
+    }
+
+    private fun observeData() {
+        viewModel.isNextBtnEnable.flowWithLifecycle(lifecycle)
+            .onEach {
+                binding.btnNext.isEnabled = it
+            }.launchIn(lifecycleScope)
+    }
+
+    private fun isValid() = isNickNameValid && isEmailValid
 
     override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
         val view = currentFocus
