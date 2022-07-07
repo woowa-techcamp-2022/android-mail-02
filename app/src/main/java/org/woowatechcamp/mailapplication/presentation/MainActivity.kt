6ca4bb4673627468041b7a47c3fdf42f93941479
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-08 04:56:00 +0900

    [FEAT] #6 - MainActivity 화면전환까지 구현

--- app/src/main/java/org/woowatechcamp/mailapplication/presentation/MainActivity.kt
+++ app/src/main/java/org/woowatechcamp/mailapplication/presentation/MainActivity.kt
@@ -1,19 +1,46 @@
 package org.woowatechcamp.mailapplication.presentation
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import org.woowatechcamp.mailapplication.R
+import org.woowatechcamp.mailapplication.data.model.PRIMARY
+import org.woowatechcamp.mailapplication.data.model.PROMOTION
+import org.woowatechcamp.mailapplication.data.model.SOCIAL
 import org.woowatechcamp.mailapplication.databinding.ActivityMainBinding
 import org.woowatechcamp.mailapplication.presentation.mail.MailFragment
 import org.woowatechcamp.mailapplication.presentation.setting.SettingFragment
 import org.woowatechcamp.mailapplication.util.base.BindingActivity
 import org.woowatechcamp.mailapplication.util.ext.replace
 
+@AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        replaceFragment(R.id.menu_mail)
         initNavigation()
         initBottomNavigation()
+        observeData()
+    }
+
+    private fun observeData() {
+        viewModel.selectCategory.flowWithLifecycle(lifecycle)
+            .onEach {
+                setItemSelect(it)
+            }
+            .launchIn(lifecycleScope)
+
+        viewModel.selectMenu.flowWithLifecycle(lifecycle)
+            .onEach {
+                binding.bnvMain.menu.findItem(it).isChecked = true
+                replaceFragment(it)
+            }
+            .launchIn(lifecycleScope)
     }
 
     private fun initNavigation() {
@@ -23,16 +50,34 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
             }
 
             nvMail.setNavigationItemSelectedListener {
-                it.isChecked = true
                 dlMail.close()
+
+                viewModel.setMenuSelect(R.id.menu_mail)
+                viewModel.setCategorySelect(
+                    when (it.itemId) {
+                        R.id.item_primary -> PRIMARY
+                        R.id.item_social -> SOCIAL
+                        else -> PROMOTION
+                    }
+                )
                 true
+            }
+        }
+    }
+
+    private fun setItemSelect(category: Int) {
+        with(binding.nvMail.menu) {
+            when (category) {
+                PRIMARY -> findItem(R.id.item_primary).isChecked = true
+                SOCIAL -> findItem(R.id.item_social).isChecked = true
+                else -> findItem(R.id.item_promotion).isChecked = true
             }
         }
     }
 
     private fun initBottomNavigation() {
         binding.bnvMain.setOnItemSelectedListener {
-            replaceFragment(it.itemId)
+            viewModel.setMenuSelect(it.itemId)
             true
         }
     }
@@ -40,16 +85,29 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     private fun replaceFragment(itemId: Int) {
         when (itemId) {
             R.id.menu_mail -> replace<MailFragment>(R.id.container_main)
-            else -> replace<SettingFragment>(R.id.container_main)
+            else -> {
+                viewModel.setCategorySelect(PRIMARY)
+                replace<SettingFragment>(R.id.container_main)
+            }
         }
     }
 
     override fun onBackPressed() {
-        with(binding.dlMail) {
-            if (isOpen)
-                close()
-            else
-                super.onBackPressed()
+        with(binding) {
+            if (bnvMain.menu.findItem(R.id.menu_setting).isChecked)
+                viewModel.setMenuSelect(R.id.menu_mail)
+            else {
+                with(dlMail) {
+                    if (isOpen)
+                        close()
+                    else {
+                        if (nvMail.menu.findItem(R.id.item_primary).isChecked)
+                            super.onBackPressed()
+                        else
+                            viewModel.setCategorySelect(PRIMARY)
+                    }
+                }
+            }
         }
     }
 }
