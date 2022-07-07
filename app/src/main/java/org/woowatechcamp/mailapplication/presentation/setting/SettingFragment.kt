3b70d847d0a67f9dc08a4779ea9b2241f07b4c1e
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-08 06:24:33 +0900

    [FEAT] #8 - 이전 화면에서 데이터 받아오기

--- app/src/main/java/org/woowatechcamp/mailapplication/presentation/setting/SettingFragment.kt
+++ app/src/main/java/org/woowatechcamp/mailapplication/presentation/setting/SettingFragment.kt
@@ -1,9 +1,23 @@
 package org.woowatechcamp.mailapplication.presentation.setting
 
+import android.os.Bundle
+import android.view.View
 import org.woowatechcamp.mailapplication.R
+import org.woowatechcamp.mailapplication.data.model.EMAIL
+import org.woowatechcamp.mailapplication.data.model.NICKNAME
 import org.woowatechcamp.mailapplication.databinding.FragmentSettingBinding
 import org.woowatechcamp.mailapplication.util.base.BindingFragment
 
 class SettingFragment : BindingFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initData()
+    }
 
+    private fun initData() {
+        with(requireActivity().intent) {
+            binding.email = getStringExtra(EMAIL)
+            binding.nickname = getStringExtra(NICKNAME)
+        }
+    }
 }
