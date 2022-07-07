39e3030aef2a90b16df559c99dee44f8d1e83240
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-07-07 23:47:44 +0900

    [ADD] #4 - Fragment 초기화

---
+++ app/src/main/java/org/woowatechcamp/mailapplication/presentation/mail/MailFragment.kt
@@ -0,0 +1,9 @@
+package org.woowatechcamp.mailapplication.presentation.mail
+
+import org.woowatechcamp.mailapplication.R
+import org.woowatechcamp.mailapplication.databinding.FragmentMailBinding
+import org.woowatechcamp.mailapplication.util.base.BindingFragment
+
+class MailFragment : BindingFragment<FragmentMailBinding>(R.layout.fragment_mail) {
+
+}
